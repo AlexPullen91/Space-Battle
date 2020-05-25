e278612c8c1e127a05036517d21bcb88395971c0
AlexPullen91 <alex_pullen03@hotmail.com>
2020-05-25 12:25:08 +0000

    Removed score from model

--- js/classes/mc/model.js
+++ js/classes/mc/model.js
@@ -1,6 +1,5 @@
 class Model { // central storage locaton for all of the games data
     constructor() { // where all of the variables and data for the game will go
-        this._score = 0;
         this.soundOn = true;
         this._musicOn = true;
         //this.gameOver = false;
@@ -15,15 +14,5 @@ class Model { // central storage locaton for all of the games data
 
     get musicOn() {
         return this._musicOn;
-    }
-
-    set score(val) { // lets me know when the score is updated
-        this._score = val; // sets score locally
-        console.log("Score updated!");
-        emitter.emit(G.SCORE_UPDATED); // emits event whenever score is updated
-    }
-    
-    get score() {
-        return this._score;
     }
 }
